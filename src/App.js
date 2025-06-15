497c5cc1df1066e3624128c1ef5ae5d1494410b1
koshioi <m.k.koszalka@gmail.com>
2025-06-15 18:33:00 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import './App.css';
+import Chat from './Chat';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🧠 Chat z Jarkiem – Niezależny Badacz</h1>
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
